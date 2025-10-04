1ad63034838bc0c126f1ca990bf0b122306d7a4a
bhattShahab <aakashbhatt758@gmail.com>
2025-10-04 11:59:18 +0530

    JPQL Query, native Query

--- src/main/java/com/example/hospital_management/repository/PatientRepository.java
+++ src/main/java/com/example/hospital_management/repository/PatientRepository.java
@@ -1,7 +1,12 @@
 package com.example.hospital_management.repository;
 
+import com.example.hospital_management.entity.BloodGroupType;
 import com.example.hospital_management.entity.Patient;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,4 +16,21 @@ public interface PatientRepository extends JpaRepository<Patient,Long> {
     List<Patient> findByBirthDateOrEmail(LocalDate date, String email);
 
     List<Patient> findByNameContaining(String ran);
+
+    @Query("select p from Patient p where p.bloodGroup=?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroupType);
+
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("select p.bloodGroup , count(p) from Patient p group by p.bloodGroup")
+    List<Object[]> countEachBloodGroupType();
+
+    @Query(value = "select * from patient", nativeQuery = true)
+    List<Patient> findAllPatients();
+
+    @Transactional //need to use Transactional with Modifying
+    @Modifying
+    @Query(value="UPDATE patient SET name = :name WHERE id = :id",nativeQuery = true)
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
 }
--- src/test/java/com/example/hospital_management/HospitalManagementApplicationTests.java
+++ src/test/java/com/example/hospital_management/HospitalManagementApplicationTests.java
@@ -1,12 +1,16 @@
 package com.example.hospital_management;
 
+import com.example.hospital_management.entity.BloodGroupType;
 import com.example.hospital_management.entity.Patient;
 import com.example.hospital_management.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -20,6 +24,7 @@ class HospitalManagementApplicationTests {
 	@Autowired
 	private PatientRepository patientRepository;
 
+//	@Transactional
 	@Test
 	void testingDBConnection(){
 		List<Patient> l = patientRepository.findByName("manveer");
@@ -30,6 +35,35 @@ class HospitalManagementApplicationTests {
 
 		List<Patient> l3 = patientRepository.findByNameContaining("Ran");
 		System.out.println(l3);
+
+		List<Patient> l4 = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+
+		for(Patient p: l4){
+			System.out.println(p);
+		}
+
+		List<Patient> l5 = patientRepository.findByBornAfterDate(LocalDate.parse("2002-09-18"));
+
+		for(Patient p : l5){
+			System.out.println(p);
+		}
+
+		List<Object[]> l6 = patientRepository.countEachBloodGroupType();
+
+		for(Object[] o :l6){
+			System.out.println(Arrays.toString(o));
+		}
+
+		List<Patient> l7 = patientRepository.findAllPatients();
+
+		for(Patient p : l7){
+			System.out.println(p);
+		}
+		int rowsUpdated = patientRepository.updateNameWithId("Majnu",6L);
+
+		System.out.println(rowsUpdated);
+
+
 	}
 
 }
